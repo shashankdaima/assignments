f275868f8b8a048561a913fd67f64807574e1d8c
Shashank Daima <daimashashank10@gmail.com>
2024-01-07 05:48:39 +0530

    palindrome done

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -1,9 +1,21 @@
-/*
-  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
-  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
-*/
-
 function isPalindrome(str) {
+  let lowerCaseStr = "";
+  const regex = /[a-zA-Z0-9]/; //Use ChatGPT to get the regex lol.
+
+  for (let iStrChar of str) {
+    if (regex.test(iStrChar)) {
+      lowerCaseStr += iStrChar;
+    }
+  }
+
+  lowerCaseStr = lowerCaseStr.toLowerCase();
+
+  for (let i = 0; i < lowerCaseStr.length / 2; i++) {
+    if (lowerCaseStr[i] !== lowerCaseStr[lowerCaseStr.length - 1 - i]) {
+      return false;
+    }
+  }
+
   return true;
 }
 
